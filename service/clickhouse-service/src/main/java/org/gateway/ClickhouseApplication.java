2fe92ec958cf27d74ed8c29030bf6ae59dc619c3
duwei <duwei@zhidaoauto.com>
2024-11-09 18:24:32 +0800

    clickhouse

---
+++ service/clickhouse-service/src/main/java/org/gateway/ClickhouseApplication.java
@@ -0,0 +1,15 @@
+package org.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class, DataSourceAutoConfiguration.class})
+public class ClickhouseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClickhouseApplication.class, args);
+    }
+}
